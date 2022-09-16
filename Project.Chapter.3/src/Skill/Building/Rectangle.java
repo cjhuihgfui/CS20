2b39cde93729f1c2a84fc0267142d2316cb7aed2
45178002 <45178002@L815WD220020.edu.cbe.ab.ca>
2022-09-16 10:48:13 -0600

    10:48

--- Project.Chapter.3/src/Skill/Building/Rectangle.java
+++ Project.Chapter.3/src/Skill/Building/Rectangle.java
@@ -1,19 +1,33 @@
 package Skill.Building;
 
+import java.util.Scanner;
+
 public class Rectangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner userinput = new Scanner(System.in);
 		
-		int length = 0;
-		int width = 0;
+		int length = 13;
+		int width = 4;
 		int perimeter;
+		int z = 2 + 2;
+		char m ='A';
 		
-		perimeter =  width + length;
+		
+		
+		perimeter =  width * length;
 		
 		System.out.println("enter width and length of a rectangle:");
-		System.out.println("this is your perimeter" + perimeter);
+		
+		System.out.println(  "+"  );
+		
+		int numEntered = userinput.nextInt();
+		
+		
+		
+		System.out.println("this is your perimeter" +  perimeter);
 		
 		
 		
